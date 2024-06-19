46a7f1d474561d560b88dc680b03392d66007239
Manjushree H C <manjushreehc4@gmail.com>
2024-06-19 14:23:11 +0530

    Initial commit

---
+++ base/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from base.models import Category, Customer, Medicine, Order, OrderItem, Prescription, Covid,Address
+
+# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Category)
+
+admin.site.register(Medicine)
+admin.site.register(Covid)
+admin.site.register(Prescription)
+admin.site.register(OrderItem)
+admin.site.register(Address)
